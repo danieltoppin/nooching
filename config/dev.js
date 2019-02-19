c6012b42b425912e8e45752b5d693611d91e85a1
Daniel Toppin <Daniel@DESKTOP>
2019-02-19 08:04:11 +0000

    removing dev.js from .gitignore

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "794917179196-643g0o6dngkil6vbliqfo1kid6f4ssoe.apps.googleusercontent.com",
+  googleClientSecret: "JucukgT5m4n8YG_2nIhJ_9Tv",
+  mongoURI:
+    "mongodb://daniel:password123@ds125945.mlab.com:25945/reactserver-dev",
+  cookieKey: "osidfoiakljfdljsdaflfuioheuipkfdlkfdlgjkdfkjsadwwdd"
+};
